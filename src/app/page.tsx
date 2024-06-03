9dfe092f8f3aba321f9b199e0a9f9d6d3804cf05
Senya <user@MacBook-Pro-res-16.local>
2024-06-03 14:17:27 +0300

    final commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,9 +14,7 @@ export default function Home() {
         <MaxWidthWrapper className="pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52 ">
           <div className="col-span-2 px-6 lg:px-0 lg:pt-4 ">
             <div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
-              <div className="absolute w-28 left-0 -top-20 hidden lg:block">
-                <img src="/snake-1.png" width={100} height={100}></img>
-              </div>
+              
               <div className="relative w-fit tracking-light text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">
                 <h1>
                   Your image on a{" "}
